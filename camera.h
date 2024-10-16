ad974b7192a652a19d75bdf6ead15d6ee016a1cd
chinX22 <eliastessema33@gmail.com>
2024-10-16 13:03:26 -0400

    added more materials, camera positioning, and depth of feild

--- camera.h
+++ camera.h
@@ -10,8 +10,14 @@ class camera{
 		double aspect_ratio = 1.0;
 		int width = 100;
 		int samples_per_pixel = 10; // Count of random samples for each pixel
-		int max_depth = 10;
-		double focal_length = 1;
+		int max_depth = 10;			// Number of times a ray can bounce
+		double vfov = 90;			//vertical view angle, quake time
+		point3 lookfrom = point3(0, 0, 0); // Point camera is looking from
+		point3 lookat = point3(0, 0, -1);  // Point camera is lookingat
+		vec3 vup = vec3(0, 1, 0);		   // Camera's "up" direction
+		double defocus_angle = 0;	// Variation angle of rays through each pixel
+		double focus_dist = 10;		// Distance from lookfrom to perfect focus plane
+		vec3 defocus_disk_u, defocus_disk_v; //Defocus disk horizontal and vertical radii
 
 		void render(const hittable& world){
 			initialize();
@@ -53,40 +59,54 @@ class camera{
 		double pixel_samples_scale;
 		point3 camera_center;
 		point3 pixel100_loc;
-		vec3 delta_u;
-		vec3 delta_v;
+		vec3 delta_u, delta_v;
+		vec3 u, v, w;
+		vec3 defocus_disc_u, defocus_disc_v;
+
 
 		void initialize(){
 			height = int(width / aspect_ratio);
 			height = (height < 1) ? 1 : height;
 
 			pixel_samples_scale = 1.0 / samples_per_pixel;
-			camera_center = point3(0, 0, 0);
+			camera_center = lookfrom;
 
 			// Viewport settings
-			auto viewport_height = 2.0;
+			auto theta = degrees_to_radians(vfov);
+			auto h = tan(theta / 2);
+			auto viewport_height = 2 * h * focus_dist;
 			auto viewport_width = viewport_height * (double(width) / height);
 
+			// Solve u, v, and q for camera orthogonial projection
+			w = unit_vector(lookfrom - lookat); // points away from looking direction
+			u = unit_vector(cross(vup, w)); // points right
+			v = cross(w, u); // points up
+
 			// Horizontal and verical viewport vectors
-			auto viewport_u = vec3(viewport_width, 0, 0);
-			auto viewport_v = vec3(0, -viewport_height, 0);
+			auto viewport_u = viewport_width * u;
+			auto viewport_v = viewport_height * -v;
 
 			// Horizontal and vertical vectors for the distance from pixel2pixel
 			delta_u = viewport_u / width;
 			delta_v = viewport_v / height;
 
 			// Find upper left pixel
-			auto viewport_upper_left = camera_center - vec3(0, 0, focal_length)
-				- viewport_u/2 - viewport_v/2;
+			auto viewport_upper_left = camera_center - (focus_dist * w)
+										- viewport_u/2 - viewport_v/2;
 			pixel100_loc = viewport_upper_left + 0.5 * (delta_u + delta_v);
+
+			// Find camera defocus disk vectors
+			auto defocus_radius = focus_dist * tan(degrees_to_radians(defocus_angle / 2));
+			defocus_disk_u = u * defocus_radius;
+			defocus_disk_v = v * defocus_radius;
 		}
 
-		// Make a camera ray that points at a random point around the pixel i, j
 		ray get_ray(int i, int j) const{
+			// Make a ray from the defocus disk to a random point on pixel i, j
 			auto offset = sample_square();
 			auto pixel_sample = pixel100_loc + ((i + offset.y()) * delta_v)
 				+ ((j + offset.x()) * delta_u);
-			auto ray_origin = camera_center;
+			auto ray_origin = (defocus_angle <= 0) ? camera_center : defocus_disk_sample();
 			auto ray_direction = pixel_sample - ray_origin;
 
 			return ray(ray_origin, ray_direction);
@@ -95,6 +115,12 @@ class camera{
 		// Get a random point within the -.5 and .5 unit square
 		vec3 sample_square() const{
 			return vec3(random_double() - 0.5, random_double() - 0.5, 0);
+		}
+
+		// Get randome point in camera defocus disk
+		point3 defocus_disk_sample() const{
+			auto p = random_in_unit_disk();
+			return camera_center + (p[0] * defocus_disk_u) + (p[1] * defocus_disk_v);
 		}
 
 		// Return the color from the ray
--- material.h
+++ material.h
@@ -56,5 +56,42 @@ class metal : public material{
 		double fuzz; //slightly randomize the end of the reflected ray = less clear reflection
 };
 
+class dielectric : public material{
+	public:
+		dielectric(double refraction_index) : refraction_index(refraction_index) {}
+
+		bool scatter (const ray& ray_in, const hit_record rec,
+					  color& attenuation, ray& scattered )
+		const override{
+			attenuation = color(1.0, 1.0, 1.0);
+			double ri = rec.front_face ? (1.0 / refraction_index) : refraction_index;
+
+			vec3 unit_direction = unit_vector(ray_in.direction());
+			double cos_theta = fmin(dot(-unit_direction, rec.normal), 1.0);
+			double sin_theta = sqrt(1.0 - cos_theta*cos_theta);
+
+			bool can_refract = ri * sin_theta <= 1.0;
+			vec3 direction;
+			if (can_refract && !(reflectance(cos_theta, ri) > random_double())){
+				direction = refract(unit_direction, rec.normal, ri);
+			} else {
+				direction = reflect(unit_direction, rec.normal);
+			}
+
+			scattered = ray(rec.p, direction);
+			return true;
+		}
+
+	private:
+	double refraction_index;
+
+	// Shlick's approx for reflactance
+	static double reflectance(double cosine, double refraction_index){
+		auto r0 = (1 - refraction_index) / (1 + refraction_index);
+		r0 = r0 * r0;
+		return r0 + (1 - r0)*pow((1 - cosine), 5);
+	}
+};
+
 
 #endif
